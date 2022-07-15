a490ae502603133f64eecd6e9b029ab481186783
reiter24 <104468384+reiter24@users.noreply.github.com>
2022-07-15 14:25:47 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <form action="search.php" method="post">
+      pombnew
+      <a href="index.php">home</a>
+      <a href="apps.php">apps</a>
+      <a href="proxy.php">proxy</a>
+      <a href="chat.php">chat</a>
+      <a href="more.php">more</a>
+      <input name="search" type="search" placeholder="Search...">    
+    </form>
+    <hr>
+    <h1>sup</h1>
+  </body>
+</html>

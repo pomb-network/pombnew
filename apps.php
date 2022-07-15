9566b5603975ef8e03aa328676bb2ab491b5c8fc
reiter24 <104468384+reiter24@users.noreply.github.com>
2022-07-15 14:26:07 -0400

    Create apps.php

---
+++ apps.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <form action="search.php" method="post">
+      pombnew
+      <a href="index.php">home</a>
+      <a href="apps.php">apps</a>
+      <a href="proxy.php">proxy</a>
+      <a href="chat.php">chat</a>
+      <a href="more.php">more</a>
+      <input name="search" type="search" placeholder="Search..."> 
+    </form>
+    <hr>
+    <ul>
+      <li><a href="wr.php">Web Retro</a></li>
+      <li><a href="dcrd.php">Discord</a></li>
+      <li><a href="https://vm.pomb.ga">Virtual Machine</a></li>
+    </ul>
+  </body>
+</html>

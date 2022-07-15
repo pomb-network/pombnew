1ad8b423408eadf3b6a185615e8d4eabcdda286d
reiter24 <104468384+reiter24@users.noreply.github.com>
2022-07-15 14:28:08 -0400

    Create wr.php

---
+++ wr.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <form action="search.php" method="post">
+      pombnew
+      <a href="index.php">home</a>
+      <a href="apps.php">apps</a>
+      <a href="proxy.php">proxy</a>
+      <a href="chat.php">chat</a>
+      <a href="more.php">more</a>
+      <input name="search" type="search" placeholder="Search..."> 
+    </form>
+    <hr>
+<iframe src="https://binbashbanana.github.io/webretro/" name="iFrame Name" scrolling="No" height="800px" width="100%" style="border:10px solid #000000;"></iframe>
+  </body>
+</html>

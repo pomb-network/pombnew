67a340c121aa9c3e4ec310a92c303e8044e65d2b
reiter24 <104468384+reiter24@users.noreply.github.com>
2022-07-15 14:27:11 -0400

    Create proxy.php

---
+++ proxy.php
@@ -0,0 +1,26 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <form action="search.php" method="post">
+      pombnew
+      <a href="index.php">home</a>
+      <a href="apps.php">apps</a>
+      <a href="proxy.php">proxy</a>
+      <a href="chat.php">chat</a>
+      <a href="more.php">more</a>
+      <input name="search" type="search" placeholder="Search..."> 
+    </form>
+    <hr>
+    <ul>
+  <li><a href="https://snoopfrogg.ga">FrogProxy</a></li>
+  <li><a href="https://ludi.easydoesit.repl.co/">Ludicrous</a></li>
+  <li><a href="https://www.howtoread.xyz">Nebula</a></li>
+  <li><a href="https://ycrgv.org/">Holy Unblocker</a></li>
+  <li><a href="https://zesterisnotcoolatall.ga/">Ultraviolet</a></li>
+  <li><a href="https://learndigitalnow.co/">Hypertabs</a></li>
+  <li><a href="https://readingclub.ml/">Incognito</a></li>
+  <li><a href="https://goodsciencehelp.com/">Degeneracy</a></li>
+  <li><a href="https://generalmathematics.net/">General Math</a></li>
+</ul>

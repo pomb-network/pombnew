7c0d3d3c30e44b1b28c48a00882c8ab3e0bd65cb
reiter24 <104468384+reiter24@users.noreply.github.com>
2022-07-15 14:26:30 -0400

    Create chat.php

---
+++ chat.php
@@ -0,0 +1,18 @@
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <form action="search.php" method="post">
+      pombnew
+      <a href="index.php">home</a>
+      <a href="apps.php">apps</a>
+      <a href="proxy.php">proxy</a>
+      <a href="chat.php">chat</a>
+      <a href="more.php">more</a>
+      <input name="search" type="search" placeholder="Search..."> 
+    </form>
+    <hr>
+    <iframe src="https://deadsimplechat.com/dnFQUGX8X" width="100%" height="800px">iframe doesnt work. reload or somthin idk</iframe>
+  </body>
+</html>

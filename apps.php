a99a5866ccbb859d3ec117de9971c0a21487da08
reiter24 <104468384+reiter24@users.noreply.github.com>
2022-07-24 17:29:01 -0400

    Update apps.php

--- apps.php
+++ apps.php
@@ -5,12 +5,12 @@
   <body>
     <form action="search.php" method="post">
       pombnew
-      <a href="index.php">home</a>
+      <a href="/">home</a>
       <a href="apps.php">apps</a>
       <a href="proxy.php">proxy</a>
       <a href="chat.php">chat</a>
       <a href="more.php">more</a>
-      <input name="search" type="search" placeholder="Search..."> 
+      <input name="search" type="search" placeholder="Search...">    
     </form>
     <hr>
     <ul>
